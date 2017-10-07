2e7bcf1fe113aa88003df921e09029e51b112401
sanpreet-ml <32572322+sanpreet-ml@users.noreply.github.com>
2017-10-07 13:41:49 +0530

    Initial commit

---
+++ add.py
@@ -0,0 +1,8 @@
+a = 5;     #Giving the value to first number
+b= 10;     #Giving the value to second number
+c =  a+b;  #adding both the values
+print("Addition of two number is:",c); #Printing the value in the form of c variable
+if (c> b):
+   print("ok")
+else:
+ print("not ok")
